88549f689df301c1a2235d87a2a2412e630b590e
venkat8814 <venkat1488@gmail.com>
2018-06-16 12:54:17 +0530

    all files

--- src/test/java/sampletgittest/samplegittest.java
+++ src/test/java/sampletgittest/samplegittest.java
@@ -1,27 +1,28 @@
 package sampletgittest;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.Test;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 import com.steadystate.css.util.Output;
 
 public class samplegittest {
 	
 	@Test
-	public void login test()
+	public void logintest() throws IOException
 	{
 		WebDriver wd = new FirefoxDriver();
 		wd.manage().window().maximize();
 		wd.get("https://www.bing.com");
-		File src=((TakesScreenshot) wd).getScreenshotAs(Output.FILE);
-		
-		FileUtils.copyFile(src, new File("C:\\workspace1\\Samplegittest\\screenshot"));
+		File src=((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\workspace1\\Samplegittest\\screenshot\\test.jpg"));
 		
 	}
 
